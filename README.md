6895969ea510437f7b631573938573b1a67c3128
Enrique Nieto <hello@nietoarranz.com>
2021-07-17 03:45:05 +0200

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# basics
-Search basic places around
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import EuropeCapitals from './slices/EuropeCapitals'
-
-export default configureStore({
-    reducer: {
-        europeCapitals: EuropeCapitals,
-    },
-})
